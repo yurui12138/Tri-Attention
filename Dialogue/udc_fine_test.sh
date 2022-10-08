5b6ab0bb231fa0cfe135b0aa122286d91187fb8f
yurui12138 <39300912+yurui12138@users.noreply.github.com>
2022-10-08 17:02:46 +0800

    Add files via upload

---
+++ Dialogue/udc_fine_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /mnt/public/home/lvwp/anaconda3/bin/activate /mnt/public/home/lvwp/anaconda3/envs/torch
+python3 -u /mnt/public/home/lvwp/process/UDC_/FP/FP/Fine-Tuning/Response_selection.py --task ubuntu
